0d45608aa665ee5ce2e4c7201ba44f05c29ce670
willyc <willcowendrums@gmail.com>
2022-01-13 18:00:19 +0000

    core complete

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -20,12 +20,11 @@ describe("Basket", () => {
     const expected = [{
       id: 1,
       type: "Blueberry",
-      quantity: 1,
-      price: 2.99,
+      price: 2.99
      }
     ]
     //execute 
-    basket.addBagelToBasket(1, "Blueberry", 2.99)
+    basket.addBagelToBasket("Blueberry")
     const result = basket.getBasket()
     //verify
     expect(result).toEqual(expected);
@@ -35,19 +34,17 @@ describe("Basket", () => {
     const expected = [{
       id: 1,
       type: "Chocolate Chip",
-      quantity: 1,
-      price: 2.99,
+      price: 2.99
      },
      {
       id: 2,
       type: "Cinnamon",
-      quantity: 1,
-      price: 2.99,
+      price: 2.99
      }
     ]
     //execute 
-    basket.addBagelToBasket(1, "Chocolate Chip", 2.99)
-    basket.addBagelToBasket(1, "Cinnamon", 2.99)
+    basket.addBagelToBasket("Chocolate Chip", 2.99)
+    basket.addBagelToBasket("Cinnamon", 2.99)
     const result = basket.getBasket()
     //verify
     expect(result).toEqual(expected);
@@ -57,12 +54,11 @@ describe("Basket", () => {
     const expected = [{
       id: 1,
       type: "remaining bagel",
-      quantity: 1,
-      price: 2.99,
+      price: 2.99
      }
     ]
-    basket.addBagelToBasket(1, "remaining bagel", 2.99)
-    basket.addBagelToBasket(1, "Bagel to Remove", 2.99)
+    basket.addBagelToBasket("remaining bagel")
+    basket.addBagelToBasket("Bagel to Remove")
     //execute 
     basket.removeBagelFromBasket("Bagel to Remove")
     const result = basket.getBasket()
@@ -74,14 +70,13 @@ describe("Basket", () => {
     const expected = [{
       id: 1,
       type: "remaining bagel",
-      quantity: 1,
       price: 2.99,
      }
     ]
-    basket.addBagelToBasket(1, "remaining bagel", 2.99)
-    basket.addBagelToBasket(1, "Bagel to Remove 1", 2.99)
-    basket.addBagelToBasket(1, "Bagel to Remove 2", 2.99)
-    basket.addBagelToBasket(1, "Bagel to Remove 3", 2.99)
+    basket.addBagelToBasket("remaining bagel")
+    basket.addBagelToBasket("Bagel to Remove 1")
+    basket.addBagelToBasket("Bagel to Remove 2")
+    basket.addBagelToBasket("Bagel to Remove 3")
     //execute 
     basket.removeBagelFromBasket("Bagel to Remove 1")
     basket.removeBagelFromBasket("Bagel to Remove 2")
@@ -90,4 +85,170 @@ describe("Basket", () => {
     //verify
     expect(result).toEqual(expected);
   });
+  it("Add one bagel then remove it returns empty", () => {
+    //setup 
+    const expected = [
+    ]
+    //execute 
+    basket.addBagelToBasket("Blueberry")
+    basket.removeBagelFromBasket("Blueberry")
+    const result = basket.getBasket()
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Adding items to a full basket", () => {
+    //setup 
+    const expected = "Basket is full!"
+    basket.addBagelToBasket("Bagel 1")
+    basket.addBagelToBasket("Bagel 2")
+    basket.addBagelToBasket("Bagel 3")
+    basket.addBagelToBasket("Bagel 4")
+    //execute    
+    const result = basket.addBagelToBasket("This bagel won't fit in my small basket")
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("overfilling my basket results in a full basket", () => {
+    //setup 
+    const expected = [{
+      id: 1,
+      type: "Bagel 1",
+      price: 2.99,
+     },
+     {
+      id: 2,
+      type: "Bagel 2",
+      price: 2.99,
+     },
+     {
+      id: 3,
+      type: "Bagel 3",
+      price: 2.99,
+     },
+     {
+      id: 4,
+      type: "Bagel 4",
+      price: 2.99,
+     }
+    ]
+    basket.addBagelToBasket("Bagel 1")
+    basket.addBagelToBasket("Bagel 2")
+    basket.addBagelToBasket("Bagel 3")
+    basket.addBagelToBasket("Bagel 4")
+    //execute
+    basket.addBagelToBasket("Bagel 5") 
+    const result = basket.getBasket()
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Adding two of the same bagel to my basket", () => {
+    //setup 
+    const expected = [{
+      id: 1,
+      type: "Blueberry",
+      price: 2.99,
+     },
+     {
+      id: 2,
+      type: "Blueberry",
+      price: 2.99,
+     },
+    ]
+    //execute 
+    basket.addBagelToBasket("Blueberry")
+    basket.addBagelToBasket("Blueberry")
+    const result = basket.getBasket()
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Remove a bagel from basket that doesn't exist", () => {
+    //setup 
+    const expected = "This Bagel doesn't exist"
+    basket.addBagelToBasket("Cinnamon")
+    //execute 
+    const result = basket.removeBagelFromBasket("sdfsdf")
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Manager increasing the size of basket", () => {
+    //setup 
+    const expected = [{
+      id: 1,
+      type: "Bagel 1",
+      price: 2.99,
+     },
+     {
+      id: 2,
+      type: "Bagel 2",
+      price: 2.99,
+     },
+     {
+      id: 3,
+      type: "Bagel 3",
+      price: 2.99,
+     },
+     {
+      id: 4,
+      type: "Bagel 4",
+      price: 2.99,
+     },
+     {
+      id: 5,
+      type: "Bagel 5",
+      price: 2.99,
+     },
+     {
+      id: 6,
+      type: "Bagel 6",
+      price: 2.99,
+     }
+    ]
+    basket.basketSize = 6
+    basket.addBagelToBasket("Bagel 1")
+    basket.addBagelToBasket("Bagel 2")
+    basket.addBagelToBasket("Bagel 3")
+    basket.addBagelToBasket("Bagel 4")
+    basket.addBagelToBasket("Bagel 5")
+    basket.addBagelToBasket("Bagel 6")
+    //execute 
+    const result = basket.getBasket()
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Getting the price of one Bagel in my basket.", () => {
+    //setup 
+    const expected = 2.99
+    basket.addBagelToBasket("Bagel 1")
+    //execute 
+    const result = basket.getTotalOfBasket()
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Getting the price of multiple Bagels in my basket.", () => {
+    //setup 
+    const expected = 2.99 + 2.99 + 2.99 
+    basket.addBagelToBasket("Bagel 1")
+    basket.addBagelToBasket("Bagel 2")
+    basket.addBagelToBasket("Bagel 3")
+    basket.addBagelToBasket("Bagel 4")
+    basket.removeBagelFromBasket("Bagel 4")
+    //execute 
+    const result = basket.getTotalOfBasket()
+    //verify
+    expect(result).toEqual(expected);
+  });
+  it("Checking the price of a Bagel before adding it to my basket.", () => {
+    //setup 
+    const expected = {
+      "sku": "BGLO",
+      "price": "0.49",
+      "name": "Bagel",
+      "variant": "Onion"
+    }
+    //execute 
+    const result = basket.getBagelPrice("Onion")
+    //verify
+    expect(result).toEqual(expected);
+  });
+
 });
--- src/basket.js
+++ src/basket.js
@@ -1,35 +1,58 @@
+const inventory = require('./inventory.js')
+
 class Basket {
-    constructor() {
-        this.basket = [];
-        this.bagelId = 1;
-    }
+  constructor() {
+    this.basket = [];
+    this.bagelId = 1;
+    this.basketSize = 4;
+  }
 
-    addBagelToBasket(quantity, type, price) {
-        const bagel = {
-            id: this.bagelId,
-            quantity: quantity,
-            type: type,
-            price: price,
-        }
-        this.bagelId++
-        this.basket.push(bagel)
-    }
-    
-    removeBagelFromBasket(type) {
-        for (let i=0; i < this.basket.length; i++) {
-        const bagelToRemove = this.basket[i]
-        if(bagelToRemove["type"] == type) {
-        this.basket.splice(i, 1)
-        }
-        }
-        
-    }
+  addBagelToBasket(type) {
+    const bagel = {
+      id: this.bagelId,
+      type: type,
+      price: 2.99,
+    };
+    this.bagelId++;
 
+    if (this.basket.length < this.basketSize) {
+      this.basket.push(bagel);
+    } else return "Basket is full!";
+  }
 
-    getBasket() {
-        return this.basket
+  removeBagelFromBasket(type) {
+    for (let i = 0; i < this.basket.length; i++) {
+      const bagelToRemove = this.basket[i];
+      if (bagelToRemove["type"] == type) {
+        this.basket.splice(i, 1);
+        return;
+      }
     }
+    return "This Bagel doesn't exist";
+  }
 
+  getTotalOfBasket() {
+      let count = 0;
+      for (let i = 0; i < this.basket.length; i++) {
+          const bagelPrice = this.basket[i].price
+          count = count + bagelPrice
+      }
+      return count
+  }
+
+  getBagelPrice(type) {
+      for (let i = 0; i < inventory.length; i++) {
+          const bagel = inventory[i]
+          if(bagel["variant"] === type) {
+              return inventory[i]
+          }
+      }
+
+  }
+
+  getBasket() {
+    return this.basket;
+  }
 }
 
 module.exports = Basket;
